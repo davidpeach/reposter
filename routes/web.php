ea99ddcf369bfbb6891f08639e2111705fd0cbd9
David Peach <davidpea.ch>
2016-08-17 19:21:32 +0100

    Remove redundant route grouping.#

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,36 @@
 <?php
-Route::group(['middleware' => 'auth'], function () {
+
+/**
+ * All Routes for the Reposter / Messenger site
+ */
+Route::group(['domain' => env('DOMAIN_REPOSTER'), 'namespace' => 'Messenger', 'middleware' => 'auth'], function () {
+
+    Route::resource('posts', 'PostsController');
+
+    Route::resource('messages', 'MessagesController');
+
+    Route::get('posts/{id}/messages', ['as' => 'posts.messages.index', 'uses' => 'PostMessagesController@index']);
+
+    Route::get('/', function () {
+        return redirect()->route('posts.create');
+    });
+
+});
 
 
-    /**
-     * All Routes for the Reposter / Messenger site
-     */
-    Route::group(['domain' => env('DOMAIN_REPOSTER'), 'namespace' => 'Messenger'], function () {
 
-        Route::resource('posts', 'PostsController');
+/**
+ * All Routes for the Music tracker site
+ */
+Route::group(['domain' => env('DOMAIN_MUSIC_TRACKER')], function () {
 
-        Route::resource('messages', 'MessagesController');
-
-        Route::get('posts/{id}/messages', ['as' => 'posts.messages.index', 'uses' => 'PostMessagesController@index']);
-
-        Route::get('/', function () {
-            return redirect()->route('posts.create');
-        });
-
+    Route::get('/', function(){
+        return "test works";
     });
 
 
-
-    /**
-     * All Routes for the Music tracker site
-     */
-    Route::group(['domain' => env('DOMAIN_MUSIC_TRACKER')], function () {
-
-        Route::get('/', function(){
-            return "test works";
-        });
-
+    Route::group(['middleware' => 'auth'], function () {
+        //
     });
 
 });
