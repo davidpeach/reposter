8855efc387db89c44c57cd36b5ead7a45cfea3ec
David Peach <mail@davidpeach.co.uk>
2016-08-22 12:14:14 +0100

    Add URL to the view all messages table.

--- resources/views/messages/all.blade.php
+++ resources/views/messages/all.blade.php
@@ -7,6 +7,7 @@
         <thead>
             <tr>
                 <th>Post Title</th>
+                <th>Url</th>
                 <th>Scheduled For</th>
                 <th>Till then</th>
                 <th>Sent?</th>
@@ -16,6 +17,7 @@
             @foreach ($messages as $message)
             <tr>
                 <td>{{ $message->post->title }}</td>
+                <td>{{ $message->post->url }}</td>
                 <td>{{ $message->scheduled_for->format('D jS M, Y') }}</td>
                 <td>{{ $message->scheduled_for->diffForHumans() }}</td>
                 <td>{{ $message->sent }}</td>
