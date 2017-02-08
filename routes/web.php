f82210c9567ecaa23c5a714c55d5542ca7772d6e
David Peach <mail@davidpeach.co.uk>
2017-02-08 22:13:57 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,61 @@
 <?php
-Log::info('Route file');
-// Route::group(['middleware' => 'auth'], function () {
 
-//     Route::group(['prefix' => 'quantified', 'namespace' => 'Quantified'], function () {
+Route::group(['middleware' => 'auth'], function () {
 
-//         Route::get('/', function() {
-//             return 'Quantified root.';
-//         });
+    Route::group(['prefix' => 'quantified', 'namespace' => 'Quantified'], function () {
 
-//         Route::resource('listens', 'ListensController');
+        Route::get('/', function() {
+            return 'Quantified root.';
+        });
 
-//         Route::resource('tweets', 'TweetsController');
+        Route::resource('listens', 'ListensController');
 
-//     });
+        Route::resource('tweets', 'TweetsController');
 
-
-//     Route::group(['prefix' => 'messenger', 'namespace' => 'Messenger'], function () {
-
-//         Route::resource('posts', 'PostsController');
-
-//         Route::resource('messages', 'MessagesController');
-
-//         Route::post('storeCustomMessage', ['as' => 'messages.storeCustomMessage', 'uses' => 'MessagesController@storeCustomMessage']);
-
-//         Route::get('posts/{id}/messages', ['as' => 'posts.messages.index', 'uses' => 'PostMessagesController@index']);
-
-//         Route::get('/', function () {
-//             return redirect()->route('posts.create');
-//         });
-
-//     });
-
-//     Route::get('/', function () {
-//         return view('dashboard');
-//     });
-
-//     Route::get('mailtest', function () {
-
-//         Mail::send('emails.daily-summary', [], function ($m) {
-//             $m->from('hello@app.com', 'Your Application');
-
-//             $m->to('mail@davidpeach.co.uk', 'Davidov')->subject('Your Reminder TESTTTTT moose');
-//         });
-
-//     });
-
-// });
+    });
 
 
-// // Route::any('checkin', 'Quantified\CheckinsController@store');
+    Route::group(['prefix' => 'messenger', 'namespace' => 'Messenger'], function () {
+
+        Route::resource('posts', 'PostsController');
+
+        Route::resource('messages', 'MessagesController');
+
+        Route::post('storeCustomMessage', ['as' => 'messages.storeCustomMessage', 'uses' => 'MessagesController@storeCustomMessage']);
+
+        Route::get('posts/{id}/messages', ['as' => 'posts.messages.index', 'uses' => 'PostMessagesController@index']);
+
+        Route::get('/', function () {
+            return redirect()->route('posts.create');
+        });
+
+    });
+
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
+    Route::get('mailtest', function () {
+
+        Mail::send('emails.daily-summary', [], function ($m) {
+            $m->from('hello@app.com', 'Your Application');
+
+            $m->to('mail@davidpeach.co.uk', 'Davidov')->subject('Your Reminder TESTTTTT moose');
+        });
+
+    });
+
+});
+
+
+// Route::any('checkin', 'Quantified\CheckinsController@store');
 
 Route::get('callback/foursquare', function () {
     return request()->all();
 });
 
-// Auth::routes();
+Auth::routes();
 
-// Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
 
-//Route::post('checkin', 'Quantified\CheckinsController@store');
-Route::any('checkin', function(){
-    Log::info('made it');
-});
+Route::any('checkin', 'Quantified\CheckinsController@store');
